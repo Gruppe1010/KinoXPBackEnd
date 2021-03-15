036468f065ac31363e69a72fa2a9946fcd53b729
TobiasBojesen <tobiasbojesen@hotmail.dk>
2021-03-15 10:55:09 +0100

    Ny SDK

--- src/main/java/com/example/gruppe10/models/users/Customer.java
+++ src/main/java/com/example/gruppe10/models/users/Customer.java
@@ -2,6 +2,8 @@ package com.example.gruppe10.models.users;
 
 import com.example.gruppe10.models.Booking;
 
+import java.util.ArrayList;
+
 public class Customer extends User{
     private ArrayList<Booking> bookingList;
 
