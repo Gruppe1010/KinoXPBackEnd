be8b1b7be331b421d0f63b13980bb35548d696a8
Vibs <vibej@hotmail.com>
2021-03-15 14:51:30 +0100

    postMapping til customers lavet

--- src/main/java/com/example/gruppe10/controllers/CustomerRESTController.java
+++ src/main/java/com/example/gruppe10/controllers/CustomerRESTController.java
@@ -1,4 +1,39 @@
 package com.example.gruppe10.controllers;
 
+import com.example.gruppe10.models.users.Customer;
+import com.example.gruppe10.repositories.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class CustomerRESTController {
+
+    // Autowired implementerer interfacet og opretter en instans af implementationen som gemmes i customerRepository
+    @Autowired
+    CustomerRepository customerRepository;
+
+
+
+
+    // nu skal den indsætte en person i vores db
+    /* Han vil have at vi bruger postman her til at teste denne postmapping
+
+        I postmand == GET --> skriv en url der giver en list<Person> tilbage --> body --> raw --> JSON --> JSON og så send
+        Nu vil han have at vi kopierer det som står nederst op i den øverste tekststed
+     */
+    @PostMapping(value="/customers", consumes = "application/json")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Customer postPerson(@RequestBody Customer customer){
+
+        // den gør at personen bliver gemt ned i repository'et
+        // save == TROR den kigger på om den er der i forvejen
+        return customerRepository.save(customer);
+    }
+
+
+
 }
